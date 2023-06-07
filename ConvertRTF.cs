803a2fdfe7553caa66db119ca8ee45d04aa16eb2
anastasiaoreskina <anastasiaoreskina@CCF1>
2023-06-08 00:55:16 +0300

    Добавьте файлы проекта.

---
+++ ConvertRTF.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Documents;
+
+namespace Post
+{
+    class ConvertRTF
+    {
+        public void saveRTFfile(string filename, TextPointer start, TextPointer end)
+        {
+            TextRange range = new TextRange(start, end);
+            FileStream fstream = new FileStream(filename, FileMode.OpenOrCreate);
+            range.Save(fstream, DataFormats.Rtf);
+            fstream.Close();
+        }
+        public void loadRTFfile(string filename, TextPointer start, TextPointer end)
+        {
+            TextRange range = new TextRange(start, end);
+            FileStream fstream = new FileStream(filename, FileMode.OpenOrCreate);
+            range.Load(fstream, DataFormats.Rtf);
+            fstream.Close();
+        }
+    }
+}
---
+++ FoldersAndLetters.xaml.cs
@@ -0,0 +1,39 @@
+﻿using ImapX.Collections;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Post
+{
+    /// <summary>
+    /// Логика взаимодействия для FoldersAndLetters.xaml
+    /// </summary>
+    public partial class FoldersAndLetters : Window
+    {
+        private CommonFolderCollection folders = ImapHelper.GetFolders();
+        MessageCollection messages;
+        public FoldersAndLetters()
+        {
+            InitializeComponent();
+            foreach(var folder in folders)
+            {
+                FolderCont.Items.Add(folder.Name);
+            }
+        }
+
+        private void FolderCont_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            messages = ImapHelper.GetMessagesForFolder(FolderCont.SelectedItem.ToString());
+        }
+    }
+}
